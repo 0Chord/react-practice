b83e7223ae761430b1f77f1047f64c78a9a1afd8
0Chord <seaweed.0chord@gmail.com>
2023-01-03 20:32:09 +0900

    useEffect, Deps 사용법 학습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
 function App() {
-  return (
-    <div>
-    <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
-    </div>
-  );
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("i run only once");
+    }, []);
+
+    useEffect(() => {
+       console.log("I run when 'keyword' changes")
+    }, [keyword]);
+    return (
+        <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
